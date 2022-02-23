c3e3771b1b73ba6f82d43991cd095f9af008ba17
rakhmadbudiono <rakhmadbudiono211@gmail.com>
2022-02-23 21:12:25 +0700

    feat: implement hello world API

--- main.go
+++ main.go
@@ -2,8 +2,23 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/rakhmadbudiono/duck-pic-service/config"
+	"github.com/rakhmadbudiono/duck-pic-service/handler"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
-	fmt.Println("Hello world!")
+	cfg := config.New()
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/", handler.Hello)
+
+	addr := fmt.Sprintf("%s:%s", cfg.Server.Host, cfg.Server.Port)
+	e.Logger.Fatal(e.Start(addr))
 }
