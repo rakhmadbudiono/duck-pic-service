a1528d381f504fe2d55d2df5d24cc10915ef9f2c
rakhmadbudiono <rakhmadbudiono211@gmail.com>
2022-03-05 14:25:44 +0700

    docs: fix FirstImage comment

--- handler/image.go
+++ handler/image.go
@@ -4,7 +4,7 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-// FirstImage handle request for root endpoint "/image"
+// FirstImage handle request for endpoint "/first"
 func FirstImage(c echo.Context) error {
 	return c.File("static/image.jpg")
 }
