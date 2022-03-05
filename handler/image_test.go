eec394e730a1c843d401ca1adb68aae30c1cecc5
rakhmadbudiono <rakhmadbudiono211@gmail.com>
2022-03-05 14:23:43 +0700

    chore: add first image test

---
+++ handler/image_test.go
@@ -0,0 +1,27 @@
+package handler_test
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/labstack/echo/v4"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFirstImage(t *testing.T) {
+	// expected result
+	expectedStatusCode := http.StatusOK
+
+	e.GET("/first", func(c echo.Context) error {
+		return c.File("../static/image.jpg")
+	})
+
+	req, _ := http.NewRequest("GET", "/first", nil)
+	rec := httptest.NewRecorder()
+	e.ServeHTTP(rec, req)
+
+	assert.Equal(t, expectedStatusCode, rec.Code, fmt.Sprintf("TestFirstImage: status code should be %d, but got %d", expectedStatusCode, rec.Code))
+}
