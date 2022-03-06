3888a8a7cfb7db82d5feaecc7d150fcc7665ccba
rakhmadbudiono <rakhmadbudiono211@gmail.com>
2022-03-06 11:15:34 +0700

    feat: get duck image with id

--- handler/image.go
+++ handler/image.go
@@ -1,10 +1,25 @@
 package handler
 
 import (
+	"fmt"
+	"net/http"
+
+	"github.com/rakhmadbudiono/duck-pic-service/package/do"
+
 	"github.com/labstack/echo/v4"
 )
 
 // FirstImage handle request for endpoint "/first"
 func FirstImage(c echo.Context) error {
 	return c.File("static/image.jpg")
+}
+
+// GetImage handler request for endpoint "/image/:id"
+func GetImage(c echo.Context) error {
+	id := c.Param("id")
+
+	path := fmt.Sprintf("%s/%s.jpg", cfg.Duck.SpaceFolder, id)
+	bytes, mimeType := do.GetObject(path)
+
+	return c.Blob(http.StatusOK, mimeType, bytes)
 }
--- handler/root.go
+++ handler/root.go
@@ -1,9 +1,19 @@
 package handler
 
-import "github.com/labstack/echo/v4"
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/rakhmadbudiono/duck-pic-service/config"
+)
+
+var cfg *config.Config
+
+func init() {
+	cfg = config.New()
+}
 
 // RegisterEndpoints register endpoint and its handler
 func RegisterEndpoints(e *echo.Echo) {
 	e.GET("/", Hello)
 	e.GET("/first", FirstImage)
+	e.GET("/image/:id", GetImage)
 }
