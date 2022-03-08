6ada10c0e9b1db6cb67425523dabd1f132383994
rakhmadbudiono <rakhmadbudiono211@gmail.com>
2022-03-08 21:35:39 +0700

    chore: remove kube manifest provision from tf

--- main.tf
+++ main.tf
@@ -4,12 +4,6 @@ terraform {
       source  = "digitalocean/digitalocean"
       version = "~> 2.0"
     }
-
-    kubectl = {
-      source  = "gavinbunney/kubectl"
-      version = ">= 1.7.0"
-    }
-
   }
 
   backend "s3" {}
@@ -17,13 +11,6 @@ terraform {
 
 provider "digitalocean" {
   token = var.digitalocean_token
-}
-
-provider "kubectl" {
-  load_config_file       = false
-  host                   = digitalocean_kubernetes_cluster.kube_cluster.endpoint
-  token                  = digitalocean_kubernetes_cluster.kube_cluster.kube_config.0.token
-  cluster_ca_certificate = base64decode(digitalocean_kubernetes_cluster.kube_cluster.kube_config.0.cluster_ca_certificate)
 }
 
 resource "digitalocean_kubernetes_cluster" "kube_cluster" {
@@ -36,13 +23,4 @@ resource "digitalocean_kubernetes_cluster" "kube_cluster" {
     size       = var.node_pool_size
     node_count = var.node_pool_node_count
   }
-}
-
-data "kubectl_filename_list" "manifests" {
-  pattern = "./manifests/*.yaml"
-}
-
-resource "kubectl_manifest" "kube_manifest" {
-  count     = length(data.kubectl_filename_list.manifests.matches)
-  yaml_body = file(element(data.kubectl_filename_list.manifests.matches, count.index))
 }
